0ec19ef6beedcde9cb632d32aff9e9c82dd34560
SagheerAbbas041 <sagheerabbas700@gmail.com>
2026-09-05 18:02:50 +0500

    Fix backend URL assignment order in index.js

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomin = "http://localhost:5000" || process.env.REACT_APP_BACKEND_URL
+const backendDomin = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
 const SummaryApi = {
     signUP : {
